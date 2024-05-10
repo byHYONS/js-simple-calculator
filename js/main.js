0a8371414f1d1d40ae1e42ce0a584b4069866857
byHYONS <byhyons@gmail.com>
2024-05-10 20:40:46 +0200

    [UPD]

--- js/main.js
+++ js/main.js
@@ -1,64 +1,77 @@
 // reset
 'use strict';
 
+/* ************************************
+            VARIABILI
+************************************ */
+
 // salvo in variabili elementi che mi servono per far funzionare la calcolatrice:
-let primoOperatore = null;
-let secondoOperatore = null;
+let primoOperatore;
+let secondoOperatore;
 let cifraCorrente = [];
-console.log(cifraCorrente);
+let operatoreMatematico;
+let risultatoCalcolato;
 
 const displayCalcolatrice = document.querySelector('.display span');
 const bottoni = document.querySelector('.btn-calcolator');
 
 
+/* ************************************
+            LOGICA PRINCIPALE
+************************************ */
 
-// faccio l'ascolto su tutti i bottoni
-bottoni.addEventListener('click', function(e){
-    const numero = e.target.dataset.number;
-    const operatore = e.target.dataset.operator
-    const uguale = e.target.dataset.equals
-    const cancella = e.target.dataset.canc;
+// FUNZIONE PRINCIPALE:
+function calcolatriceMain(){
 
-    numeroDigitato(numero);
-    operatoreDigitato(operatore);
-    risultatoDigitato(uguale);
-    cancellaDigitato(cancella);
-    
+    // faccio l'ascolto su tutti i bottoni
+    bottoni.addEventListener('click', function(e){
+        const numero = e.target.dataset.number;
+        const operatore = e.target.dataset.operator;
+        const uguale = e.target.dataset.equals;
+        const cancella = e.target.dataset.canc;
+
+        // invoco funzioni
+        numeroDigitato(numero);
+
+        operatoreDigitato(operatore);
+
+        risultatoDigitato(uguale)
+
+        cancellaDigitato(cancella);
+
+        console.log('sono la cifra ' + cifraCorrente);
+        console.log('ho salvato il ' + primoOperatore);
+        console.log('ho salvato il ' + secondoOperatore);   
+        
+    });
+}
 
 
 
-    
-})
-
-
-
-// importo il valore numerico dall'event listener:
+// INPORTO IL VALORE E SALVO SULL'ARREY:
 function numeroDigitato(input){
-    console.log('sono un numero ' + input);
-    const valoreCorrente = parseInt(input);
-    console.log(valoreCorrente);
+    const valoreCorrente = parseInt(input);;
 
-    const controlloZero = [];
-    if (!isNaN(valoreCorrente)) controlloZero.push(valoreCorrente);
-    console.log({controlloZero});
+    if (!isNaN(input)) cifraCorrente.push(valoreCorrente);
+    if (cifraCorrente[0] == 0 && cifraCorrente.length > 1) cifraCorrente.shift();
 
-
-    // if (!isNaN(input)) cifraCorrente.push(valoreCorrente);
-    if (controlloZero[0] !== 0) cifraCorrente.push(controlloZero);
-
-    console.log(cifraCorrente);
-    schermo(valoreCorrente);
-
-    
+    console.log({cifraCorrente});
+    schermo(valoreCorrente);  
 
 }
 
 
 
 
-// importo il valor dell'operatore dall'event listener:
+// IMPORTO IL VALORE
 function operatoreDigitato(input){
-    console.log('sono un operatore ' + input);
+    
+    if (input === '+' || input === '-' || input === '*' || input === '/' ) {
+        primoOperatore = cifraCorrente.join('');
+        cifraCorrente = [];
+        operatoreMatematico = input;
+        console.log({operatoreMatematico});
+    }
 
 
 }
@@ -69,145 +82,79 @@ function operatoreDigitato(input){
 
 // importo il valore dell'uguale dall'event listener:
 function risultatoDigitato(input){
-    console.log('sono un risultato ' + input);
+    
+    // salvo il l'arrey nella variabile secondo operatore:
+    if (input === '=') {
+        secondoOperatore = cifraCorrente.join('');
+        console.log(secondoOperatore);
 
-}
-
-
-
-
-
-// importo il valore del cancella dall'event listener:
-function cancellaDigitato(input){
-    console.log('sono un cancellatore ' + input);
-    if (input === 'delete') {
-        displayCalcolatrice.textContent = 0;
-        cifraCorrente = [];
     }
 
-
+    // invoco la funzione per calcolare il risultato:
+    calcolaRisultato(input)
 }
 
+
+// funzione per calcolare il risultato:
+function calcolaRisultato(input){
+    switch (true){
+        case input === '=' && operatoreMatematico === '+':
+            risultatoCalcolato = parseInt(primoOperatore) + parseInt(secondoOperatore);
+            displayCalcolatrice.textContent = risultatoCalcolato;
+            break;
+        case input === '=' && operatoreMatematico === '-':
+            risultatoCalcolato = parseInt(primoOperatore) - parseInt(secondoOperatore);
+            displayCalcolatrice.textContent = risultatoCalcolato;
+            break;
+        case input === '=' && operatoreMatematico === '*':
+            risultatoCalcolato = parseInt(primoOperatore) * parseInt(secondoOperatore);
+            displayCalcolatrice.textContent = risultatoCalcolato;
+            break;
+        case input === '=' && operatoreMatematico === '/':
+            if (parseInt(secondoOperatore) === 0) {          // gestisco l'eventuale divisione per 0 dando l'errore
+                displayCalcolatrice.textContent = 'error';
+            } else {
+                risultatoCalcolato = parseInt(primoOperatore) / parseInt(secondoOperatore);
+                displayCalcolatrice.textContent = risultatoCalcolato;
+            }
+            break;
+    }
+}
 
 
 // creo funzione per il display:
-function schermo(input,){
+function schermo(input){
     const cifraNumerica = cifraCorrente.join('');
 
-    cancellaDigitato()
-    console.log({input});
-    let digita = true;
+    // cancellaDigitato()
+    // console.log({input});
     if (input.length === 0){
         displayCalcolatrice.textContent = 0;
     } else if (cifraNumerica.length > 11) {
         displayCalcolatrice.textContent = cifraNumerica + ' e';
+    } else if (isNaN(input)){
+        // non fare niente...
     } else {
         displayCalcolatrice.textContent = cifraNumerica;
+        // console.log(cifraNumerica);
+        // console.log(cancellaDigitato);
     }
 }
 
+// importo il valore del cancella dall'event listener:
+function cancellaDigitato(input){
+    // console.log('sono un cancellatore ' + input);
+    if (input === 'delete') {
+        displayCalcolatrice.textContent = 0;
+        cifraCorrente = [];
+    }
+}
+
+calcolatriceMain();
+
+// FINE
 
 
 
 
-
-
-
-// const bottoneCanc = document.querySelector('.canc')
-// const bottoneUguale = document.querySelector('.equals');
-
-
-
-
-
-
-
-
-// // definisco una funzione per leggere i bottoni numerici:
-// function ascoltonBottoniNumeri(){
-//     const bottoniNumerici = document.querySelectorAll('.num');
-//     // per farmi restituire il valore di tutti i bottoni selezionati:
-//     for (let index = 0; index < bottoniNumerici.length; index++) {
-//         const button = bottoniNumerici[index];
-//         // per avere l'ascolto su tutti i bottoni selezionati:
-//         button.addEventListener('click', function() {
-//             const numero = parseInt(button.textContent);
-//             aggiornaDisplay(numero);
-//             calcolaOperazione(numero);
-//             console.log(numero);
-//         })   
-//     }
-// } 
-
-
-// // // definisco una funzione per leggere gli operatori:
-// function ascoltonBottoniOperatore(){
-//     const bottoniOperatori = document.querySelectorAll('.operator');
-//     // per farmi restituire il valore di tutti i bottoni selezionati:
-//     for (let index = 0; index<bottoniOperatori.length; index++){
-//         const button = bottoniOperatori[index];
-//         // per avere l'ascolto su tutti i bottoni selezionati:
-//         button.addEventListener('click', function(){
-//             const operatore = button.textContent;
-//             convertiOperatore(operatore)
-//             aggiornaDisplay(operatore);
-//             calcolaOperazione(operatore)
-//             console.log(operatore);
-//         })
-//     }
-// }
-
-//  // funzione per le operazioni:
-//  function calcolaOperazione(input){
-//     let valoreCorrente;
-//     if (input !== clearDisplay()) {
-//         cifraCorrente.push(input);
-//         valoreCorrente = parseInt(cifraCorrente.join(''));
-//         } else {
-//             valoreCorrente = 0;
-            
-//     }
-
-//     console.log(valoreCorrente);
-
-    
-//     }
-    
-
-
- 
- 
- 
-//  // Funzione per aggiornare il display della calcolatrice
-//  function aggiornaDisplay(input) {
-//      // if (input === '-' || input === '-' || input === 'x' || input === '/') {
-//          //     display.textContent = 0;
-//          calcolaOperazione(input);
-//          // } else {
-
-//              // Aggiunge l'input al display corrente
-             
-             
-//              //}
-//              console.log(display.textContent); // Per debugging
-//             }
-
-
-
-            
-// // funzione per ripulire il display:
-// function clearDisplay(){
-//     canc.addEventListener('click', function(){
-//         display.textContent = 0;
-//         calcolaOperazione()
-//     })
-// }
-//             // Funzione per inizializzare la calcolatrice
-
-//     ascoltonBottoniNumeri();
-//     ascoltonBottoniOperatore();
-//     clearDisplay()
-
-// Chiamata della funzione di inizializzazione al caricamento della pagina
-// document.addEventListener('DOMContentLoaded', inizializzaCalcolatrice);
 
